6a02b698315e872b86df043fa028fda7aa705fa8
Vianey Rivera <vianeyr8624@gmail.com>
2026-04-04 14:08:44 -0400

    Install all neccessary packages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl underline">
+          Welcome to My Game Design Portfolio
+        </h1>
+        <p className="text-lg">This is a showcase of my work.</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
